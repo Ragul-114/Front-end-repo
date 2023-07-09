46334e047a92d764f562e3e37925b53a51683caf
Ragul K R <137045302+Ragul-114@users.noreply.github.com>
2023-07-09 15:55:59 +0530

    Add files via upload

---
+++ home2.js
@@ -0,0 +1,226 @@
+import React from "react";
+import App from "./App";
+import Home from "./home";
+import './index.css';
+import {Link} from "react-router-dom";
+import header2 from './images/header2.jpg';
+import ngp from './images/ngp.jpg';
+import cs4 from './images/cs4.jpg';
+import rlearn2 from './images/rlearn 2.jpg';
+import clg from './images/clg.jpg';
+import clg1 from './images/clg1.jpg';
+import asc from './images/asc.jpg';
+import ngp5 from './images/ngp5.jpg';
+import clg6 from './images/clg6.jpg';
+import c3 from './images/c3.jpg';
+import c44 from './images/c44.jpg';
+import compuer from './images/compuer.jpg';
+import chairman from './images/chairman.jpg';
+import clg4 from './images/clg4.jpg';
+import pg from './images/pg.jpg';
+import mb4 from './images/mb4.jpg';
+import rlearn21 from './images/rlearn 21.jpg'; 
+import v from './images/course2.jpg';
+
+
+function Home2(){
+return(
+    <div>
+  <div class="imgh2">
+  <img src={header2} style={{height:"60vh",width:"99vw"}} alt="frontpage"/>
+    </div>    
+    <br></br><br></br><br></br>
+
+<div class="imgh2"style={{position:"relative",left:"637px"}}>
+<img src={asc} style={{height:"36vh",width:"34vw"}}/>
+</div>
+
+ <text style={{fontSize:"35px"}}>
+  &nbsp;  <h2 style={{fontSize:"47px",padding:"8px"}}>About NGPASC</h2>
+  &nbsp;&nbsp;<text style={{textDecoration:"underline"}}>Dr.N.G.P Arts and Science College</text> was established by the Kovai Medical Center Research and Educational Trust &nbsp;&nbsp;Coimbatore in 1997. The college incepted its educational journey with 4
+  Under Graduate Programmes and today it is &nbsp;&nbsp;emerged as one of the major academies in self-financing institutions in Tamil Nadu, India.
+The college conferred with the &nbsp;&nbsp;autonomous status in 2015-2016 by the UGC and is affiliated to Bharathiar University, Coimbatore. The College was &nbsp;&nbsp;accredited by the NAAC 
+with ‘A++’ Grade in the Third cycle. Our college is consecutively ranked at the national  level &nbsp;&nbsp;within 100 ranks by NIRF and currently at 88th position in India.
+ </text>
+ <br></br><br></br><br></br><br></br>
+ <br></br> <br></br><br></br><br></br><br></br>
+ <div class="polaroidh2" style={{height:"89vh",position:"relative",left:"155px",width:"82%"}}>
+  <div class="imgh2">
+  <img src={chairman} style={{height:"79vh",width:"29vw",position:"relative",left:"10px",top:"10px"}}/>
+  </div>
+  <text style={{fontSize:"35px"}}><text style={{fontSize:"81px",fontFamily:"Optima",position:"absolute",top:"49px",left:"860px"}}>Chairman <br></br><text style={{fontSize:"30px"}}>
+</text></text>
+<ul style={{listStyleType:"square"}}>
+<text style={{fontSize:"37.5px",position:"absolute",top:"200px",left:"650px"}}><lih2>Chairman & Managing Director, <a href="https://kmchhospitals.com/" style={{color:"red",textDecoration:"none"}}>Kovai Medical Center and Hospital</a>, Coimbatore
+<br></br></lih2>
+<lih2> Kovai Medical Center Research and Educational Trust<br></br></lih2>
+ <lih2><a href="http://www.kmchcon.ac.in/" style={{color:"red",textDecoration:"none"}}>KMCH College of Nursing</a><br></br></lih2>
+ <lih2><a href="https://www.kmchcop.ac.in/" style={{color:"red",textDecoration:"none"}}>KMCH College of Pharmacy</a><br></br></lih2>
+ <lih2><a href="http://www.kmchphysiotherapy.ac.in/" style={{color:"red",textDecoration:"none"}}>KMCH College of Physiotherapy</a><br></br></lih2>
+ <lih2><a href="https://www.kmchcot.ac.in/" style={{color:"red",textDecoration:"none"}}>KMCH College of Occupational Therapy</a><br></br></lih2>
+ <lih2><a href="https://www.drngpasc.ac.in/" style={{color:"blue",textDecoration:"none"}}>Dr.N.G.P Arts and Science College</a><br></br></lih2>
+ <lih2><a href="https://www.drngpit.ac.in/"style={{color:"blue",textDecoration:"none"}}>Dr.N.G.P Institute of Technology</a><br></br></lih2>
+
+ <lih2><a href="http://www.drngpeducation.ac.in/"style={{color:"blue",textDecoration:"none"}}>Dr.N.G.P College of Education</a><br></br></lih2>
+ <lih2><a href="http://www.drngpeducation.ac.in/"style={{color:"blue",textDecoration:"none"}}>THE NGP SCHOOL&emsp;</a><br></br></lih2>
+
+ </text>
+ </ul>
+  <text style={{fontSize:"46px",position:"absolute",top:"780px",left:"49px",fontFamily:"Cambria"}}>
+  Dr.Nalla G. Palaniswami</text>
+ </text>
+ </div>
+ <br></br>
+ <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+
+<div class="imgh2">
+<img src={clg4} style={{height:"83.4vh",width:"260vh",position:"relative",top:"28px"}}/>
+</div>
+<br></br><br></br><br></br><br></br>
+<br></br><br></br><br></br><br></br>
+<div class="imgh2">
+<img src={clg6} style={{height:"40vh",width:"29vw",position:"relative",left:"680px"}}/>
+</div>
+ <h1 style={{fontSize:"47px",left:"5px"}}>Courses offered</h1><br></br>
+<text style={{fontSize:"35px"}}>
+&nbsp; The college, at present offers 31 UG, 17 PG and 25 Research (M.Phil. & Ph.D.) Programmes, 04 PG Diploma, 7Diploma and &nbsp;&nbsp;58 Value Added Courses 
+under 32 well established departments through 
+six variant faculties. Our college has an &nbsp;&nbsp;intellectual capital of more than 360+ academically well experienced teaching fraternity amongst
+200+ faculty members &nbsp;&nbsp;are doctorates and they cater to the needs of 7500 students on roll. The primary principle of the college is to impart &nbsp;&nbsp;standard education together with discipline and 
+moral values in order to make the students efficient, skilled,and &nbsp;&nbsp;innovative and adopt themselves in the digital global arena.
+<br></br>
+<br></br>
+<br></br><br></br>
+<div class="polaroidh2" style={{width:"99.5%",height:"3%",left:"5px",backgroundColor:"black",padding:"0px",position:"relative"}}>
+<text style={{fontSize:"4px",padding:"0px"}}>.</text>
+</div>
+<br></br>
+
+<img src={mb4} style={{width:"86vh",left:"88px",top:"3px",height:"53vh",position:"relative"}}/>
+
+<img src={pg} style={{width:"86vh",position:"relative",left:"183px",height:"52.5vh"}}/>
+
+<br></br>
+<div class="polaroidh2" style={{height:"90vh",width:"40vw",left:"110px",position:"relative"}}>
+  <div class="imgh2">
+  </div>
+  <br></br> 
+  <div class="poloh2" style={{backgroundColor:"#2e2e2d",color:"white",textAlign:"center",fontSize:"35px"}}>
+  <text style={{fontSize:"45px"}}> &nbsp;Computer Science</text>
+  </div><br></br>
+  <div class="ul" style={{position:"relative",left:"20px",listStyleType:"square"}}>
+<lih2 style={{padding:"8px"}}>
+    <a href="https://www.drngpasc.ac.in/best-bsc-msc-computer-science-college-in-coimbatore-tamilnadu" style={{textDecoration:"none"}}>
+      <text style={{fontFamily:"monospace",fontSize:"37px"}}>
+      &nbsp;&nbsp;Bsc.Computer science&emsp;&emsp;&emsp;&emsp;&emsp;</text></a>
+      <br></br>
+      </lih2>
+<lih2 style={{padding:"8px"}}><a href="https://www.drngpasc.ac.in/best-bsc-msc-computer-science-data-analytics-college-in-coimbatore-tamilnadu" style={{textDecoration:"none"}}>
+      <text style={{fontFamily:"monospace",fontSize:"37px"}}> &nbsp;&nbsp;Bsc.Computer science &emsp;&emsp;&emsp;&emsp;<br></br><text style={{fontSize:"37px"}}>&nbsp;&nbsp;&nbsp;with Data analytics</text></text></a>
+     <br></br>
+     </lih2>
+<lih2 style={{padding:"8px"}}>
+      <a href="https://www.drngpasc.ac.in/best-arts-college-for-bsc-computer-technology--in-coimbatore-tamilnadu" style={{textDecoration:"none"}}>
+      <text style={{fontFamily:"monospace",fontSize:"37px",color:"blue"}}> &nbsp;&nbsp;Bsc.Computer Technology&emsp;&emsp;<br></br></text></a>
+     </lih2>
+<lih2 style={{padding:"8px"}}>
+     <text style={{fontFamily:"monospace",fontSize:"37px"}}> <a href="https://www.drngpasc.ac.in/best-arts-college-for-bsc-information-technology-in-coimbatore-tamilnadu" style={{textDecoration:"none"}}>
+       &nbsp;&nbsp;BCA&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+      <br></br></a></text>
+   </lih2>
+<lih2 style={{padding:"8px"}}>
+      <a href="https://www.drngpasc.ac.in/best-arts-college-for-bsc-information-technology-in-coimbatore-tamilnadu" style={{textDecoration:"none"}}>
+      <text style={{fontFamily:"monospace",fontSize:"37px"}}> &nbsp;&nbsp;Bsc.Information Technology<br></br></text></a>
+     </lih2>
+<lih2 style={{padding:"8px"}}>
+      <a href="https://www.drngpasc.ac.in/best-arts-college-for-bsc-information-technology-in-coimbatore-tamilnadu" style={{textDecoration:"none"}}>
+      <text style={{fontFamily:"monospace",fontSize:"37px"}}>  &nbsp;&nbsp;Bsc Artificial Intelligence and &emsp;&emsp;&emsp;&emsp;Machine Learning<br></br></text></a>
+      </lih2>
+  </div>
+</div>
+
+<div class="polaroidh2" style={{height:"90vh",width:"40vw",position:"absolute",top:"5193px",left:"1035px"}}>
+  <div class="imgh2">
+  </div>
+<br></br> 
+<div class="poloh2" style={{backgroundColor:"#260e40",color:"white",textAlign:"center",fontSize:"30px"}}>
+  <text style={{fontSize:"45px"}}> &nbsp;Computer Science</text>
+  </div><br></br>
+ <div style={{left:"10px"}}>
+      <lih2 style={{padding:"8px"}}>
+    <a href="https://www.drngpasc.ac.in/best-bsc-msc-computer-science-college-in-coimbatore-tamilnadu" style={{textDecoration:"none"}}>
+      <text style={{fontFamily:"monospace",fontSize:"37px"}}>
+      Msc.Computer science &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</text></a></lih2><br></br><br></br>
+      <lih2 style={{padding:"8px"}}>
+    <a href="https://www.drngpasc.ac.in/best-bsc-msc-computer-science-college-in-coimbatore-tamilnadu" style={{textDecoration:"none"}}>
+      <text style={{fontFamily:"monospace",fontSize:"37px"}}>
+        Msc.Computer science with Data analytics</text></a></lih2>
+</div>
+</div>
+</text><br></br><br></br>
+
+ <h1 style={{fontSize:"37px",borderStyle:"solid double",width:"100%"}}>
+ <h2 style={{backgroundColor:"black",color:"white"}}>&nbsp;DEPARTMENTS</h2>
+ <div style={{margin:"5px",position:"relative",top:"11px"}}><h3 style={{position:"relative",fontSize:"48px",textShadow:"1.4px 1.5px orange",top:"29px"}}>&nbsp;COMPUTER STREAM</h3>
+ <div class="imgh2">
+ <img src={c3} style={{height:"7.5vh",width:"5.3vw",top:"26px",position:"absolute",left:"480px"}}/>
+ </div>
+
+
+
+ <div class="polaroidh2" style={{height:"82vh",position:"relative",left:"129px",width:"85%",top:"80px",boxShadow:"inset 0px 0px 0px 10px blue"}}>
+   <div>
+    <div class="imgh2">
+    <img src={compuer} style={{height:"80vh",width:"31vw",top:"2px",padding:"5px",backgroundColor:"white"}}/>
+    </div>
+   <nav style={{position:"absolute",top:"595px",width:"50%",fontSize:"4px",left:"700px",backgroundColor:"black"}}>j</nav>
+  <div class="imgh2">
+  <img src={c44} style={{position:"absolute",left:"1043px",top:"547px",height:"9.5vh",width:"4.7vw"}}/>
+  </div>
+   <h1 style={{left:"810px",top:"36px",fontSize:"75px",position:"absolute",fontFamily:"times"}}>Computer Science</h1><br></br>
+   <text style={{fontSize:"36px",fontFamily:"canbria",position:"absolute",top:"240px",left:"600px"}}>&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;With a zeal and motto of expert guidance and 
+   education <br></br>&emsp;&nbsp;we also assist the students to attain excellent placement in &emsp;&emsp;&emsp; reputed Multinational Companies.We also 
+   provide &emsp;&emsp;&emsp;various pathways to actively engage the students in &emsp;&emsp;&emsp;&emsp;&emsp;various academic & non-academic related <br></br>&emsp;&emsp;&emsp;&emsp;
+   &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;activities</text>
+   <text style={{fontSize:"27px",fontFamily:"Cambria",position:"absolute",top:"660px",left:"677px"}}>“We need to understand that if we all work on inclusion 
+  together, it's <br></br>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;going to be faster & <br></br>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+    also efficient!"
+   </text>
+ </div>
+ 
+ </div>
+<div style={{position:"relative"}}>
+  <br></br><br></br>
+<div class="backhover">
+  
+<nav style={{backgroundColor:"black",fontSize:"24px",position:"relative",width:"100%",top:"294px"}}>h</nav>
+
+<text style={{position:"absolute",left:"556px",fontSize:"33px",fontFamily:"serif",top:"300px"}}>DEPARTMENT OF &emsp;&emsp;&emsp;&emsp;&emsp;
+  &emsp;&nbsp; COMPUTER SCIENCE</text>
+  <div class="imgh2 polo" style={{backgroundColor:"white"}}>
+    <Link to="/home">
+    <img src={cs4} style={{height:"28vh",width:"15vw",top:"20px",left:"809px",position:"relative"}}/>
+      </Link> 
+  </div>
+</div>
+</div>
+
+    </div>
+    </h1>
+
+ <br></br>
+<br></br>
+<br></br>
+ <div class="back1">
+  <div class="imgh2" style={{position:"relative",left:"760px"}}>
+  <img src={ngp5} style={{width:"19vw",height:"18vh",position:"relative",top:"1px"}}/>
+  </div>
+  <h1 style={{fontSize:"49px",color:"white",borderStyle:"dotted dashed solid double",width:"70px",position:"absolute",top:"54px",left:"93px"}}>We provide quality education in terms of Eternal Wisdom !</h1>
+ </div>
+   
+   
+  </div>
+   );
+   }
+   export default Home2;
